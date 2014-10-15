4e0a652831433a9b8e1f3739b20dc58830bdac36
Simon Randby <simon.randby+github@gmail.com>
2014-10-15 23:22:36 +0200

    Problem 3 now completely solved
    Added some comments to make it neater

--- D/source/problems/projectEulerProblem3.d
+++ D/source/problems/projectEulerProblem3.d
@@ -2,26 +2,41 @@ import std.stdio, std.math, std.array;
 import utilities.sieveOfEratosthenes;
 
 void main(){
+	//Initialize sieveclass
 	sieveOfErastothenes mySieve = new sieveOfErastothenes();
+	//Variables
 	real numberToBeFactorized, currentFactor;
 	real[] primes, factors;
 	real primeSizeLimit = 512;
 	numberToBeFactorized = 600851475143;
 	currentFactor = 1;
+
+	//Make initial pool of primes to check against
 	primes = mySieve.primesLessThanN(primeSizeLimit);
+
+	//Begin iterating
 	for(int i = 0; i < primes.length; i++){
 		currentFactor = primes[i];
+		//Check if the i-th prime is a factor
 		if(numberToBeFactorized % currentFactor == 0) {
+			//If so, append it to the list of known factors
 			factors ~= currentFactor;
+			//Make the number one's trying to factorize to the largest known factor (reduces problem size massively)
 			numberToBeFactorized = numberToBeFactorized / currentFactor;
+			//If the larges known factor now is 1 stop the trying
 			if(numberToBeFactorized == 1) break;
+			//Check the i-th prime against the new factor again, to see if it is a factor more than once
 			i--;
 		}
+		/*If one has exhaused the current set of primes,
+		but isn't confident that the problem is solved,
+		increase the size of the set of primes one tests,
+		and restart the looping*/
 		if(i == primes.length - 1) {
 			if(numberToBeFactorized > primeSizeLimit){
-				i = -1;
 				primeSizeLimit = primeSizeLimit + primeSizeLimit;
 				primes = mySieve.primesLessThanN(primeSizeLimit);
+				i = -1;
 			}
 		}
 	}
