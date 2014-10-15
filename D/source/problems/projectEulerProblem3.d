3ebf1a25074987369667273c965e475288f3d639
Simon Randby <simon.randby+github@gmail.com>
2014-10-15 20:39:32 +0200

    further work on problem 3

--- D/source/problems/projectEulerProblem3.d
+++ D/source/problems/projectEulerProblem3.d
@@ -1,9 +1,21 @@
-import std.stdio;
+import std.stdio, std.math, std.array;
+import utilities.sieveOfEratosthenes;
 
 void main(){
-	int largestPrimeFactor = 0;
-	int numberToBeFactorized = 600851475143;
+	sieveOfErastothenes mySieve = new sieveOfErastothenes();
+	//int largestPrimeFactor = 0;
+	float numberToBeFactorized = 600851475143;
+	int squareRootOfNumberToBeFactorized = cast(int)floor(sqrt(numberToBeFactorized));
+	int[] primeBase = mySieve.primesLessThanN(squareRootOfNumberToBeFactorized + 1);
+	int[] factors;
 
+	writeln("Number: ", numberToBeFactorized, ", sqrt: ", squareRootOfNumberToBeFactorized, ", primebase: ", primeBase);
 
-	writeln(largestPrimeFactor);
+	for(int i = 0; i < primeBase.length; i++){
+		if(numberToBeFactorized % primeBase[i] == 0){	
+			factors ~= primeBase[i];
+		}
+	}
+	//writeln(largestPrimeFactor);
+	writeln(factors.back);
 }
