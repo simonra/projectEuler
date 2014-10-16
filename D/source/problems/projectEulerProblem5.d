831a1053a0f2a2414a7ca167abb5642af9af16d9
Simon Randby <simon.randby+github@gmail.com>
2014-10-16 12:55:33 +0200

    Added some comments, now also prints whole number

--- D/source/problems/projectEulerProblem5.d
+++ D/source/problems/projectEulerProblem5.d
@@ -1,16 +1,21 @@
-import std.stdio, std.algorithm;
+import std.stdio, std.algorithm, std.conv;
 import utilities.sieveOfEratosthenes;
 import utilities.primeFactorization;
 
 void main(){
+	//Initialize other classes
 	sieveOfEratosthenes mySieve = new sieveOfEratosthenes();
 	primeFactorization factorizationUtil = new primeFactorization();
 
+	//variables
 	real productOfFactorBase = 1;
 	real[] primeBase = mySieve.primesLessThanN(20);
 	real[] factorBase, factorsOfCurrentNumber;
 	real numberOfTimesToAddPrime;
 	
+	/*Because 2 and 3 are inevitably going to be factors of other numbers less than 20
+	one might as well start at 4 (same argument could be made for 6, but performance 
+	is not that critical at this problem size).*/
 	for(int i = 4; i <= 20; i++){
 		factorsOfCurrentNumber = factorizationUtil.eratosthenesFactorization(i);
 		foreach(prime; primeBase){
@@ -22,9 +27,11 @@ void main(){
 			}
 		}
 	}
+	//Compute the final number
 	foreach(factor; factorBase){
 		productOfFactorBase *= factor;
 	}
+	//Had to convert to int to not get scientiffic notation but whole number =(
 	writeln("FactorBase is: ", factorBase);
-	writeln("Smallest possible product: ", productOfFactorBase);
+	writeln("Smallest possible product: ", to!int(productOfFactorBase));
 }
