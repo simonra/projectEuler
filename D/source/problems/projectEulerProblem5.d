f50062d3d10ccaf4a40fce8933dd03f451e11936
Simon Randby <simon.randby+github@gmail.com>
2014-10-16 02:04:15 +0200

    Started problem 5

---
+++ D/source/problems/projectEulerProblem5.d
@@ -0,0 +1,28 @@
+import std.stdio, std.algorithm;
+import utilities;
+
+void main(){
+	sieveOfEratosthenes mySieve = new sieveOfEratosthenes();
+	primeFactorization factorizationUtil = new primeFactorization();
+
+	real productOfFactorBase = 1;
+	real[] primeBase = mySieve.primesLessThanN(20);
+	real[] factorBase, factorsOfCurrentNumbers;
+	real numberOfTimesToAddPrime;
+	
+	for(int i = 4; i <= 20; i++){
+		factorsOfCurrentNumbers = factorizationUtil.eratosthenesFactorization(i);
+		foreach(prime; primeBase){
+			if(factorBase.count(prime) < factorsOfCurrentNumbers.count(prime)){
+				numberOfTimesToAddPrime = factorsOfCurrentNumbers.count(prime) - factorBase.count(prime);
+				for(int j = 0; j < numberOfTimesToAddPrime; j++){
+					factorBase ~= prime;
+				}
+			}
+		}
+	}
+	foreach(factor; factorBase){
+		productOfFactorBase *= factor;
+	}
+	writeln("Smallest possible product: ", productOfFactorBase);
+}
